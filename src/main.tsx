3d280eabc52dbd745c472b2da024908f5526daaf
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-04-25 14:06:56 +0300

    added pages & components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+// React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Component
 import App from './App.tsx'
+// Styles
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
