c47abf6a3a021eca4876ae8c68dd835b793f4d14
dattali18 <74492594+dattali18@users.noreply.github.com>
2024-04-26 17:12:26 +0300

    added fuzzy search to the libraries page

--- src/pages/libraries/libraries.tsx
+++ src/pages/libraries/libraries.tsx
@@ -4,16 +4,20 @@ import { getAllLibraries } from "../../api/librariesApi.ts";
 import LibraryItem from "../../components/libraryItem/libraryItem.tsx";
 
 import Media from "../../api/media.ts";
+import Library from "../../api/library.ts";
 
 const Libraries = () => {
-  const [libraries, setLibraries] = useState([]);
+  const [libraries, setLibraries] = useState([] as Library[]);
   const [loading, setLoading] = useState(true);
+
+  const [filteredLibraries, setFilteredLibraries] = useState([] as Library[]);
 
   useEffect(() => {
     const fetchLibraries = async () => {
       setLoading(true);
       const libraries = await getAllLibraries();
       setLibraries(libraries);
+      setFilteredLibraries(libraries);
       setLoading(false);
     };
     fetchLibraries();
@@ -22,12 +26,25 @@ const Libraries = () => {
   return (
     <div>
       <h1>Libraries</h1>
+      {/* adding an text filed for searching through the libraries */}
+      <input
+        type="text"
+        placeholder="Search Library"
+        onChange={(e) => {
+          const searchTerm = e.target.value;
+          setFilteredLibraries([...libraries]);
+          const filteredLibraries = libraries.filter((library: { name: string }) => {
+            return library.name.toLowerCase().includes(searchTerm.toLowerCase());
+          });
+          setFilteredLibraries(filteredLibraries);
+        }}
+      />
       {loading ? (
         <p>Loading...</p>
       ) : (
         <ul>
-          {libraries?.length > 0 ? (
-            libraries.map((library: { id: number; name: string, keywords: string, media: Media[] }) => (
+          {filteredLibraries?.length > 0 ? (
+            filteredLibraries.map((library: { id: number; name: string, keywords: string, media: Media[] }) => (
               <li key={library.id}>
                 <LibraryItem 
                 id={library.id}
